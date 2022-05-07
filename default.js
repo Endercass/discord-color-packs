33cfacefab1a4bb8b8eeb2c8de3990b19fc02ef5
Endercass <46408156+Endercass@users.noreply.github.com>
2022-05-07 09:34:42 -0400

    Create default.js

---
+++ default.js
@@ -0,0 +1,3 @@
+document.documentElement.style.setProperty('--background-primary', '#36393f');
+document.documentElement.style.setProperty('--background-secondary', '#2f3136');
+document.documentElement.style.setProperty('--background-tertiary', '#202225');
